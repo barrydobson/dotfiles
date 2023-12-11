5c5f989b691b7c5cdc29dc0618f5395411d77007
barrydobson <github@barrydobson.me>
2023-12-11 10:19:46 +0000

    Squashed 'vendor/github.com/DanielThomas/oh-your-dotfiles/' content from commit 22d5395
    git-subtree-dir: vendor/github.com/DanielThomas/oh-your-dotfiles
    git-subtree-split: 22d539580c1682c9e3498bd61acd60aa7bd26ff7

---
+++ defaults/path.zsh
@@ -0,0 +1,3 @@
+source "${0:a:h}/homebrew.zsh"
+BREW_PATH="$HOMEBREW_PREFIX/bin:$HOMEBREW_PREFIX/sbin:/usr/local/bin:/usr/local/sbin"
+export PATH=$BREW_PATH:$PATH
---
+++ lib/install-arch.zsh
@@ -0,0 +1,36 @@
+#!/usr/bin/env zsh
+libdir=${0:a:h}
+source $libdir/dotfiles.zsh
+source $libdir/terminal.zsh
+source $libdir/homebrew.zsh
+source $libdir/mas.zsh
+source $libdir/git.zsh
+
+function run_installers() {
+  brew_install_upgrade_formulas
+  mas_install_upgrade_formulas
+
+  dotfiles_find_installer install.sh | while read installer ; do run "running ${installer}" "${installer}" ; done
+
+  for file_source in $(dotfiles_find_installer install.open); do
+    OLD_IFS=$IFS
+    IFS=$'\n'
+    basedir="$(dirname $file_source)"
+    for file in `cat $file_source`; do
+      canonical_file="$basedir/$file"
+      open_file "$canonical_file"
+    done
+    IFS=$OLD_IFS
+  done
+}
+
+function run_postinstall() {
+  dotfiles_find_installer post-install.sh | while read installer ; do run "running ${installer}" "${installer}" ; done
+}
+
+function main() {
+  run_installers
+  run_postinstall
+}
+
+main "$@"
